1bac498cc429122583866ac0db716d2a39b0ff47
Muhamad Ghufron Fadlullah <cuci.kasut.bangsar@gmail.com>
2026-06-03 09:52:00 +0800

    adding ansible semaphore docker files
    adding ansible semaphore docker files

---
+++ ansible-semaphore-docker/containers/hello-world/src/test/java/com/titan/hello/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titan.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
